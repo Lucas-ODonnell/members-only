211da622ac6c1faae04df10dff6ceebdebee1dcc
Lucas <lucas.odonnelll@gmail.com>
2021-02-10 14:39:50 -0800

    updated gemfile and made user model with devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_213222) do
+ActiveRecord::Schema.define(version: 2021_02_10_223449) do
 
   create_table "posts", force: :cascade do |t|
-    t.string "author", null: false
-    t.string "title", limit: 7, null: false
-    t.text "body", limit: 20, null: false
+    t.string "author"
+    t.string "title", limit: 7
+    t.text "body", limit: 20
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "posts", "users"
