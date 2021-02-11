04e68973cb484f03a7fd86d272dfe1db149415a3
Lucas <lucas.odonnelll@gmail.com>
2021-02-10 16:55:40 -0800

    fixed broken user model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,24 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
+  end
+
+  def show
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params) 
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
